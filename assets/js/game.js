d013359b43326c1e316217b95ca103faaafcb2d6
nyagrodha <nyagrodha@me.com>
2020-06-02 17:49:56 -0500

    first initial commit

---
+++ assets/js/game.js
@@ -0,0 +1,14 @@
+// this creates a window prompting user input
+var playerName = window.prompt("What's your robot's name?");
+console.log(playerName);
+console.log("This logs a string, good for leaving yourself a message");
+// this will do math and log 20
+console.log(10 + 10);
+// what's this?
+console.log("Our robot's name is " + playerName);
+
+// this creates a function named fight 
+function fight() {
+    window.alert("The fight has begun!");
+}
+// fight();
