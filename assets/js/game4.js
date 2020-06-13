791d939ea37acd853ec233514b307e4f96ca801d
nyagrodha <nyagrodha@me.com>
2020-06-13 18:26:56 -0500

    start and endGame functionalities added

--- assets/js/game4.js
+++ assets/js/game4.js
@@ -56,6 +56,10 @@ var fight = function (enemyName) {
 }
 
 var startGame = function () {
+  // reset player stats
+  playerHealth = 100;
+  playerAttack = 10;
+  playerMoney = 10;
   for (var i = 0; i < enemyNames.length; i++) {
     if (playerHealth > 0) {
       window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
@@ -67,6 +71,27 @@ var startGame = function () {
       break;
     }
   }
+  // play again
+  // startGame();
+  // endGame();
 };
 
 startGame();
+
+var endGame = function () {
+  // if player is still alive, player wins!
+  if (playerHealth > 0) {
+    window.alert("Great job! Your robot's survived the game! You have a score of " + playerMoney + ".");
+  } else {
+    window.alert("The game has now ended. Let's see how you did!");
+  }
+   // ask the player if they'd like to play again
+   var playAgainConfirm = window.confirm("Would you like to play again?");
+   if (playAgainConfirm) {
+     // restart the game
+     startGame();
+   } else {
+     window.alert("Thank you for playing Robot Gladiators! Come back soon!");
+   }
+ }
+ endGame();
