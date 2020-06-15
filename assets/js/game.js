20478a56fc7a093b8ba1d1f7aeed4dd84fec0b40
nyagrodha <nyagrodha@me.com>
2020-06-14 22:41:57 -0500

    objectifying intialized

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,32 +1,6 @@
-var playerInfo = {
-  name: window.prompt("What's your 🤖robot's name?"),
-  health: 100,
-  attack: 10,
-  money: 10,
-}
-
-// You can also log multiple values at once like this
-console.log("Your robot, " + playerInfo.name + ", packs an attack value of " + playerInfo.attack + ", and has " + playerInfo.health + "% power remaining.");
-
-var enemyInfo = [
-  {
-    name: "Roborto🤖",
-    attack: 12
-  },
-  {
-    name: "Amy🤖Android",
-    attack: 13
-  },
-  {
-    name: "Robo🤖Trumble",
-    attack: 14
-  }
-];
-
-var fight = function (enemyName) {
-  console.log(enemy);
+var fight = function (enemy) {
   // repeat and execute as long as the enemy robot is alive
-  while (enemyHealth > 0 && playerInfo.health > 0) {
+  while (enemy.health > 0 && playerInfo.health > 0) {
     // ask the user if they wish to fight or to skip the fight.
     var promptFight = window.prompt("Would you like to FIGHT or to SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
     // moving skip condition here
@@ -47,25 +21,25 @@ var fight = function (enemyName) {
       }
     }
     console.log(promptFight);
-    //Subtract the value of `playerInfo.attack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
+    //Subtract the value of `playerInfo.attack` from the value of `enemy.health` and use that result to update the value in the `enemy.health` variable
     // if player chooses to fight, then fight
-    enemyHealth = enemyHealth - playerInfo.attack;
+    enemy.health = enemy.health - playerInfo.attack;
     // Log a resulting message to the console so we know that it worked.
-    console.log(playerInfo.name + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + "% power remaining.");
+    console.log(playerInfo.name + " attacked " + enemy.name + ". " + enemy.name + " now has " + enemy.health + "% power remaining.");
     // check enemy's health
-    if (enemyHealth <= 0) {
-      window.alert(playerInfo.name + " has destroyed " + enemyName);
+    if (enemy.health <= 0) {
+      window.alert(playerInfo.name + " has destroyed " + enemy.name);
       break;
     } else {
-      window.alert(enemyName + " still has " + enemyHealth + "% power remaining.");
+      window.alert(enemy.name + " still has " + enemy.health + "% power remaining.");
     }
-    // Subtract the value of `enemyAttack` from the value of `playerInfo.health` and use that result to update the value in the `playerInfo.health` variable.
-    playerInfo.health = playerInfo.health - enemyAttack;
+    // Subtract the value of `enemy.attack` from the value of `playerInfo.health` and use that result to update the value in the `playerInfo.health` variable.
+    playerInfo.health = playerInfo.health - enemy.attack;
     // Log a resulting message to the console so we know that it worked.
-    console.log(enemyName + " attacked " + playerInfo.name + ". " + playerInfo.name + " now has " + playerInfo.health + "% power remaining.");
+    console.log(enemy.name + " attacked " + playerInfo.name + ". " + playerInfo.name + " now has " + playerInfo.health + "% power remaining.");
     // check player's health
     if (playerInfo.health <= 0) {
-      window.alert(enemyName + " has decimated " + playerInfo.name + ".");
+      window.alert(enemy.name + " has decimated " + playerInfo.name + ".");
       break;
     } else {
       window.alert(playerInfo.name + " still has " + playerInfo.health + "% power remaining.");
@@ -79,27 +53,11 @@ var shop = function () {
   switch (shopOptionPrompt) {
     case "REFILL": // new case
     case "refill":
-      if (playerInfo.money >= 7) {
-        window.alert("Refilling " + playerInfo.name + "'s power by 20% for 7 trinkets.");
-
-        // increase health and decrease money
-        playerInfo.health = playerInfo.health + 20;
-        playerInfo.money = playerInfo.money - 7;
-      } else {
-        window.alert("You don't have enough trinkets at this time.");
-      }
+      playerInfo.refillHealth();
       break;
     case "UPGRADE": // new case
     case "upgrade":
-      if (playerInfo.money >= 7) {
-        window.alert("Upgrading " + playerInfo.name + "'s attack by 6 for 7 trinkets.");
-
-        // increase attack and decrease money
-        playerInfo.attack = playerInfo.attack + 6;
-        playerInfo.money = playerInfo.money - 7;
-      } else {
-        window.alert("You don't have enough trinkets at this time.");
-      }
+      playerInfo.upgradeAttack();
       break;
     case "LEAVE": // new case
     case "leave":
@@ -116,17 +74,63 @@ var shop = function () {
   }
 }
 
-var randomNumber = function(min, max) {
+var randomNumber = function (min, max) {
   var value = Math.floor(Math.random() * (max - min + 1) + min);
 
   return value;
 }
 
+var playerInfo = {
+  name: window.prompt("What's your 🤖robot's name?"),
+  health: 100,
+  attack: 10,
+  money: 10,
+  reset: function () {
+    this.health = 100;
+    this.attack = 10;
+    this.money = 10;
+  },
+  refillHealth: function() {
+    if (this.money >= 7) {
+      window.alert("Charging " + playerInfo.name + "'s power by 20% for 7 trinkets.");
+      this.health += 20;
+      this.money -= 7;
+    } else {
+      window.alert("You have insufficient trinkets.");
+    }
+  },
+  upgradeAttack: function() {
+    if (this.money >= 7) {
+      window.alert("Upgrading " + playerInfo.name + "'s attack by 6 for 7 trinkets.");
+      this.attack += 6;
+      this.money -= 7;
+    } else {
+      window.alert("You have insuffient trinkets.");
+    }
+  }
+};
+
+// You can also log multiple values at once like this
+console.log("Your robot, " + playerInfo.name + ", packs an attack value of " + playerInfo.attack + ", and has " + playerInfo.health + "% power remaining.");
+
+var enemyInfo = [
+  {
+    name: "Roborto🤖",
+    attack: randomNumber(10, 14)
+  },
+  {
+    name: "Amy🤖Android",
+    attack: randomNumber(10, 14)
+  },
+  {
+    name: "Robo🤖Trumble",
+    attack: randomNumber(10, 14)
+  }
+];
+
 var startGame = function () {
   // reset player stats
-  playerInfo.health = 100;
-  playerInfo.attack = 10;
-  playerInfo.money = 10;
+  playerInfo.reset();
   for (var i = 0; i < enemyInfo.length; i++) {
     if (playerInfo.health > 0) {
       window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
